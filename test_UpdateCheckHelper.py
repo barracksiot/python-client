0ea86be2e2266f24ebd07ccb271ec75541f0dde2
Paul Aigueperse <paul.aigueperse@gmail.com>
2016-09-07 16:12:06 -0400

    Refacto des test en cours

--- test_UpdateCheckHelper.py
+++ test_UpdateCheckHelper.py
@@ -2,72 +2,70 @@ import json
 import hashlib
 import os
 
-import pytest
 from mock import MagicMock
+import requests_mock
 
 from barracks_sdk import UpdateDetail, UpdateDetailRequest, PackageDownloadHelper, BarracksHelper, ApiResponse
 
-@pytest.fixture()
-def init_helpers_and_mocks():
-    """
-    Create mocked objects and fake data to be used in the tests
-    """
-    callback_fake = MagicMock()
-    bh = BarracksHelper("eafeabd7a13bacf44a8122ed4f7093c5c7b356a4f567df2654984fffef2a67be",
-                         "https://barracks.ddns.net/")
-    upd_response = '{"packageInfo": {"size": 1925, "md5": "21fd9b37d2b458b42dc2e450699075ef", "url": "https://barracks.ddns.net/api/device/update/download/b640d7f2-31fd-4502-a8e1-bcf7df343f92"}, "customUpdateData": {"config": "QWERTYkeyboard"}, "versionId": "ft"}'
-    update_fake = UpdateDetail(json.loads(upd_response))
-
-    return callback_fake, bh, update_fake
+_base_url = 'https://app.barracks.io/'
+_upd_json_response = '{"packageInfo": {"size": 1925, "md5": "21fd9b37d2b458b42dc2e450699075ef", "url": "https://app.barracks.io/api/device/update/download/b640d7f2-31fd-4502-a8e1-bcf7df343f92"}, "customUpdateData": {"config": "QWERTYkeyboard"}, "versionId": "ft"}'
+_bh = BarracksHelper("eafeabd7a13bacf44a8122ed4f7093c5c7b356a4f567df2654984fffef2a67be", _base_url)
 
 
-def test_check_update_properly_calls_callback(init_helpers_and_mocks):
+def update_available_callback(result):
+    assert isinstance(result, UpdateDetail) is True
+
+
+def update_unavailable_callback(result):
+    assert isinstance(result, ApiResponse) is True
+    assert result.get_error_code() is 204
+
+
+@requests_mock.mock()
+def test_check_available_update_properly_calls_callback(m):
     """
-    Tests that the client ''check'' callback is called whatever comes from Barracks API
+    Tests that the client ''check'' callback is called with UpdateDetail when status code is available
     """
-    callback_fake = init_helpers_and_mocks[0]
-    bh = init_helpers_and_mocks[1]
+    m.post(_base_url + '/api/device/update/check', text=_upd_json_response, status_code=200)
 
     request = UpdateDetailRequest("v1", "MyDevice", "{\"AnyCustomData\":\"any_value\"}")
-    helper = bh.updateCheckerHelper
-    helper.check_update(request, callback_fake)
-    callback_fake.assert_called()
+    helper = _bh.updateCheckerHelper
+
+    helper.check_update(request, update_available_callback)
 
 
-def test_check_update_properly_calls_callback_with_good_params(init_helpers_and_mocks):
+@requests_mock.mock()
+def test_check_not_available_update_properly_calls_callback(m):
     """
-    Tests that the client ''check'' callback is called whatever comes from Barracks API
+    Tests that the client ''check'' callback is called with ApiResponse when status code is not available
     """
-    callback_fake = init_helpers_and_mocks[0]
-    bh = init_helpers_and_mocks[1]
+    m.post(_base_url + '/api/device/update/check', text='', status_code=204)
 
     request = UpdateDetailRequest("v1", "MyDevice", "{\"AnyCustomData\":\"any_value\"}")
-    helper = bh.updateCheckerHelper
-    helper.check_update(request, callback_fake)
-    callback_fake.assert_called()
+    helper = _bh.updateCheckerHelper
+
+    helper.check_update(request, update_unavailable_callback)
 
 
-def test_download_package_properly_build_request_with_good_params(init_helpers_and_mocks):
+def test_download_package_properly_build_request_with_good_params():
     """
     Tests that the request is build and send with appropriate parameters and headers
     """
-    bh = init_helpers_and_mocks[1]
-    update_detail = init_helpers_and_mocks[2]
-    helper = bh.packageDownloadHelper
+    update_detail = UpdateDetail(json.loads(_upd_json_response))
+    helper = _bh.packageDownloadHelper
     built_request = helper.build_download_request(update_detail.get_package_info().get_url())
 
     headers = built_request.headers
-    assert headers['Authorization'] == bh.get_api_key()
+    assert headers['Authorization'] == _bh.get_api_key()
     assert built_request.url == update_detail.get_package_info().get_url()
 
 
-def test_check_update_properly_build_request_with_good_params(init_helpers_and_mocks):
+def test_check_update_properly_build_request_with_good_params():
     """
     Tests that the request is build and send with appropriate parameters and headers
     """
-    bh = init_helpers_and_mocks[1]
-    request = UpdateDetailRequest("v1", "MyDevice", "{\"AnyCustomData\":\"any_value\"}")
-    helper = bh.updateCheckerHelper
+    request = UpdateDetailRequest("v1", "MyDevice", "{AnyCustomData\":\"any_value\"}")
+    helper = _bh.updateCheckerHelper
     built_request = helper.build_request(request)
     body = json.loads(built_request.body)
 
@@ -76,47 +74,54 @@ def test_check_update_properly_build_request_with_good_params(init_helpers_and_m
     assert body['customClientData'] == request.customClientData
 
     headers = built_request.headers
-    assert headers['Authorization'] == bh.get_api_key()
+    assert headers['Authorization'] == _bh.get_api_key()
 
 
-def test_download_package_properly_calls_callback(init_helpers_and_mocks):
+def test_download_package_properly_calls_callback():
     """
     Tests that the client ''download'' callback is called whatever comes from Barracks API
     """
-    callback_fake = init_helpers_and_mocks[0]
-    bh = init_helpers_and_mocks[1]
-    update_fake = init_helpers_and_mocks[2]
+    callback_fake = MagicMock()
+    update_fake = UpdateDetail(json.loads(_upd_json_response))
 
-    ph = PackageDownloadHelper(bh.get_api_key())
+    ph = PackageDownloadHelper(_bh.get_api_key())
     ph.download_package("./anyfile", update_fake, callback_fake)
     callback_fake.assert_called()
 
 
-def test_download_package_properly_calls_callback_with_good_params(init_helpers_and_mocks):
+@requests_mock.mock()
+def test_download_package_properly_calls_callback_with_good_params(m):
     """
     Tests that the client ''download'' callback is called with appropriate parameters when success
     """
-    callback_fake = init_helpers_and_mocks[0]
-    bh = init_helpers_and_mocks[1]
-    update_fake = init_helpers_and_mocks[2]
+    path_test = './anyfile'
+    final_path = './myfile'
+    test_file = open(path_test, 'a')
+    test_file.close()
+    callback_fake = MagicMock()
+    update_fake = UpdateDetail(json.loads(_upd_json_response))
 
-    ph = PackageDownloadHelper(bh.get_api_key())
-    result = ph.download_package('./anyfile', update_fake, callback_fake)
+    with open(path_test, 'r') as test_file :
 
-    real_file_path = os.path.realpath('./anyfile')
-    callback_fake.assert_called_with(real_file_path)
-    assert result == real_file_path
+        m.get(update_fake.get_package_info().get_url(), raw=test_file.read(), status_code=200)
+
+        ph = PackageDownloadHelper(_bh.get_api_key())
+        result = ph.download_package(final_path, update_fake, callback_fake)
+
+        real_file_path = os.path.realpath(final_path)
+        callback_fake.assert_called_with(real_file_path)
+
+        assert result == real_file_path
 
 
-def test_download_package_properly_calls_callback_with_error(init_helpers_and_mocks):
+def test_download_package_properly_calls_callback_with_error():
     """
     Tests that the client ''download'' callback is called with error object when fail
     """
-    callback_fake = init_helpers_and_mocks[0]
-    bh = init_helpers_and_mocks[1]
-    update_fake = init_helpers_and_mocks[2]
+    callback_fake = MagicMock()
+    update_fake = UpdateDetail(json.loads(_upd_json_response))
 
-    ph = PackageDownloadHelper(bh.get_api_key())
+    ph = PackageDownloadHelper(_bh.get_api_key())
     obj = ph.download_package('./anyfile', update_fake, callback_fake)
 
     real_file_path = os.path.realpath('./anyfile')
@@ -128,21 +133,20 @@ def test_init_barracks_helper_without_good_arguments():
     Tests that the barracks helper canot be build without API_Key = None
     """
     try:
-        BarracksHelper(None,"http://app.barracks.io")
+        BarracksHelper(None, "http://app.barracks.io")
         assert False
     except ValueError:
         assert True
 
 
-def test_download_fail_without_temporary_path(init_helpers_and_mocks):
+def test_download_fail_without_temporary_path():
     """
     Tests that the client ''download'' callback is called with appropriate parameters when success
     """
-    callback_fake = init_helpers_and_mocks[0]
-    bh = init_helpers_and_mocks[1]
-    update_fake = init_helpers_and_mocks[2]
+    callback_fake = MagicMock()
+    update_fake = UpdateDetail(json.loads(_upd_json_response))
 
-    ph = PackageDownloadHelper(bh.get_api_key())
+    ph = PackageDownloadHelper(_bh.get_api_key())
 
     result = ph.download_package('', update_fake, callback_fake)
 
