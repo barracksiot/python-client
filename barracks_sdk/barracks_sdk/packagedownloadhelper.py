6d188faa58c6e53d69a0ac57132a99ba85404629
Grégoire Weber <gregoire@clevertoday.com>
2016-09-08 17:30:08 -0400

    Fix code style of package download helper

--- barracks_sdk/barracks_sdk/packagedownloadhelper.py
+++ barracks_sdk/barracks_sdk/packagedownloadhelper.py
@@ -6,8 +6,11 @@ from apiresponse import ApiResponse
 
 
 class PackageDownloadHelper:
-    _apiKey = None
+
     CHUNK_SIZE = 1024
+    DEFAULT_TEMPORARY_PATH = '/tmp/update.tmp'
+
+    _apiKey = None
 
     def __init__(self, api_key):
         self._apiKey = api_key
@@ -18,7 +21,7 @@ class PackageDownloadHelper:
         :type temporary_path: string
         :type update_detail: UpdateDetail
         """
-        temporary_path = '/tmp/update.tmp' if temporary_path is None or temporary_path == '' else temporary_path
+        temporary_path = self.DEFAULT_TEMPORARY_PATH if temporary_path else temporary_path
 
         # Download the file
         url = update_detail.get_package_info().get_url()
@@ -38,22 +41,24 @@ class PackageDownloadHelper:
         # Accept certificate even if another domain than the Barracks one is used
         response = requests.session().send(prepared_request, verify=False, stream=True)
 
-        with open(tmp_path, 'wb') as file:
+        with open(tmp_path, 'wb') as tmp_file:
             sys.stdout.flush()
             for chunk in response.iter_content(chunk_size=self.CHUNK_SIZE):
                 if chunk:  # filter out keep-alive new chunks
-                    file.write(chunk)
+                    tmp_file.write(chunk)
         return tmp_path
 
     def build_download_request(self, url):
         """
         :type url: string
         """
-        headers = {'Authorization': self._apiKey, 'Content-Type': 'application/json'}
+        headers = {
+            'Authorization': self._apiKey,
+            'Content-Type': 'application/json'
+        }
         req = requests.Request(method='GET', url=url, headers=headers)
 
         return req.prepare()
-
 
     @staticmethod
     def check_file_integrity(file_path, md5):
